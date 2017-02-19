4762fac7eca147d728406ea274b89f2a95d4eb8d
Unknown <krikjers@stud.ntnu.no>
2017-02-19 20:21:48 +0100

    småfunksjoner lagt til + litt andre endringer
    esm_get_motor_direction(),
    esm_update_floor(),
    esm_update_motor_direction,
    esm_get_current_floor

--- esm.c
+++ esm.c
@@ -24,11 +24,12 @@ enum floors_between{
 		FLOOR2_3,
 		FLOOR3_4		
 };
-///----------States----------///
-static int esm_state = -1;	
-static int floor_number = -1;
-static int motor_direction = 0;
-static int floor_between = -1;
+///----------States----------//
+//set initial values
+static esm_states esm_state = -1;
+static floors floor_number = -1;
+static elev_motor_direction_t motor_direction = 0;
+static floors_between floor_between = -1;
 ///--------------------------///
 
 	
@@ -42,6 +43,7 @@ int esm_initialize_elevator(){
 		elev_set_motor_direction( DIRN_DOWN );	
 	} while( elev_get_floor_sensor_signal() == -1 ); //-1 == nofloor
 	elev_set_motor_direction( DIRN_STOP );	
+	//motor_direction = DIRN_DOWN;
 	esm_state = INITIALIZE;	
 	floor_number = elev_get_floor_sensor_signal();
 	printf( "Current floor = floor%d\n", floor_number );
@@ -50,29 +52,66 @@ int esm_initialize_elevator(){
 }
 
 void esm_door_closed(){
-	elev_set_door_open_lamp(0);
+	elev_set_door_open_lamp(off);
+	esm_state = IDLE;
 }
 
 void esm_open_door(){
-	elev_set_door_open_lamp(1);
+	elev_set_door_open_lamp(on);
 	timer_start();
 }
 
 void esm_floor_passed( int floor ){
+	esm_state = SERVICING_FLOOR;
 	//dummy function for testing
 }
 
 
 void esm_stop_button_pressed(){
 	printf(" Stop button is pressed!\n" );
-	//dummy function for testing
+	esm_state = EMERGENCY_STOP;
+	elev_set_stop_lamp( on );
+	elev_set_motor_direction(DIRN_STOP);
+	//op_clear_all_orders();  not implemented yet
+
+	if ( elev_get_floor_sensor_signal() == -1 ) {		//between floors
+		
+	}
+	else {												//at floor
+		elev_set_door_open_lamp( on );
+	}
+	
 }
+
 void esm_stop_button_released(){
 	printf(" Stop button is released!\n" );
-	//dummy function for testing
+	timer_start();
+	if (elev_get_floor_sensor_signal() == -1) {
+		esm_state = IDLE;
+	}
+	else {
+		esm_state = SERVICING_FLOOR;
+	}
+
+	
 }
 
 
+static elev_motor_direction_t esm_get_motor_direction() {
+	return motor_direction;
+}
+
+static void esm_update_floor(int floor_num) {
+	floor_number = floor_num;
+}
+
+static void esm_update_motor_direction(elev_motor_direction_t motor_dir) {
+	motor_direction = motor_dir;
+}
+
+static floors floors esm_get_current_floor(){
+	return floor_number;
+}
 
 
 
--- esm.h
+++ esm.h
@@ -2,15 +2,15 @@
 #include "timer.h"
 
 enum stop_status{
-		RELEASED = 1,
+		RELEASED = -1,
 		NOT_PRESSED = 0,
-		PRESSED = -1
+		PRESSED = 1
 };
 
-int current_stop_button_stauts = NOT_PRESSED;
+//int current_stop_button_stauts = NOT_PRESSED;
 int prev_stop_button_status = NOT_PRESSED;
 
-//Come to defined floor and update current state and current floor and LED
+//go to to defined floor and update current state and current floor and LED
 int esm_initialize_elevator( void );
 
 void esm_door_closed( void );
@@ -23,6 +23,13 @@ void esm_stop_button_pressed( void );
 
 void esm_stop_button_released( void );
 
+static elev_motor_direction_t esm_get_motor_direction( void );
+
+static void esm_update_floor(floors floor_num );
+
+static void esm_update_motor_direction(elev_motor_direction_t motor_dir);
+
+static floors esm_get_current_floor( void );
 
 
 
